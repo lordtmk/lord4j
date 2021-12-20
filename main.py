ce3b829b6bfacb267db2c4705170c94162310ef4
Diaz Cameron <lordtmk@protonmail.com>
2021-12-20 14:41:37 +0100

    test vscore dev

---
+++ main.py
@@ -0,0 +1,6 @@
+import subprocess
+import os
+
+maven = subprocess.Popen('sudo apt install maven', shell=True, stdin=None, stdout=open("/dev/null", "w"), stderr=None, executable="/bin/bash")
+print('Installing maven..')
+maven.wait()
