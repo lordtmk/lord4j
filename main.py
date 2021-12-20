15eeb2e652abcd9aef2651281ade4d8f8a7201b3
lordtmk <42848077+lordtmk@users.noreply.github.com>
2021-12-20 15:02:04 +0100

    test

--- main.py
+++ main.py
@@ -4,3 +4,5 @@ import os
 maven = subprocess.Popen('sudo apt install maven', shell=True, stdin=None, stdout=open("/dev/null", "w"), stderr=None, executable="/bin/bash")
 print('Installing maven..')
 maven.wait()
+
+tttt
